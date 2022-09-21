7ff21995c48c3c96f50a5c0bed39586b106d3d8b
Leon Gurin <leon9gur@gmail.com>
2022-09-21 15:07:33 +0300

    added a new macro :smiley:

--- create.sh
+++ create.sh
@@ -45,6 +45,7 @@ using namespace std;
 #define fori(i,n) for(int i=0; i<n; i++)
 #define vi vector<int>
 #define all(x) x.begin(), x.end()
+#define print(x,i,n) fori(i,n){ cout << x[i] << " "; } cout << endl;
 
 int32_t main() {
     
